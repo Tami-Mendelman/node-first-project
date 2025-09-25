240b24eb69ae53a83ef48f07a18c3078f26f25cd
Tami-Mendelman <tami5700804@gmail.com>
2025-09-25 14:58:35 +0300

    write to logger

--- middleware/middlewareBook.js
+++ middleware/middlewareBook.js
@@ -1,5 +1,18 @@
+// middleware/middlewareBook.js
+const fs = require('fs');
+const path = require('path');
+
 function logger(req, res, next) {
-     console.log("a call was made");
-  next();
+  const logMessage = `a call was made: ${req.method} ${req.url} - ${new Date().toISOString()}\n`;
+
+  // כתיבה לקובץ server.log
+  const logPath = path.join(__dirname, '../server.log');
+  fs.appendFileSync(logPath, logMessage, 'utf8');
+
+  // אם תרצי גם להדפיס למסך במקביל
+  console.log(logMessage.trim());
+
+  next(); // המשך לראוט הבא
 }
+
 module.exports = logger;
